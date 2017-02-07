ed37ed32b2bf07756629ad42da48055754d568a4
Abhi Govula <abhi.govula@scriptbees.com>
2017-02-07 14:43:45 +0530

    Initial commit

---
+++ src/js/components/todolist.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TodoItem from './todo';
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul className="todolist">
+        {this.props.items.map(item => (
+          <TodoItem key={item.id} id={item.id} text={item.text} completed={item.done} onItemCompleted={this.props.onItemCompleted} onDeleteItem={this.props.onDeleteItem} />
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodoList;
